a39ea7a45fe9f2950c5bdeb7c43aebee71328620
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-09 22:02:38 -0300

    wip

--- config/Seeds/UserSeed.php
+++ config/Seeds/UserSeed.php
@@ -2,27 +2,27 @@
 declare(strict_types=1);
 
 use Migrations\AbstractSeed;
+use Faker\Factory;
 
 /**
  * User seed.
  */
 class UserSeed extends AbstractSeed
 {
-    /**
-     * Run Method.
-     *
-     * Write your database seeder using this method.
-     *
-     * More information on writing seeds is available here:
-     * https://book.cakephp.org/phinx/0/en/seeding.html
-     *
-     * @return void
-     */
     public function run(): void
     {
         $data = [];
 
-        $table = $this->table('users');
-        $table->insert($data)->save();
+        $faker = Faker\Factory::create();
+
+        for ($i = 1; $i < 20; $i++) {
+            $data['firstName'] = $faker->firstName();
+            $data['lastName'] = $faker->lastName();
+            $data['email'] = $faker->unique()->email;
+            $data['password'] = password_hash('123', PASSWORD_DEFAULT);
+
+            $table = $this->table('users');
+            $table->insert($data)->save();
+        }
     }
 }
