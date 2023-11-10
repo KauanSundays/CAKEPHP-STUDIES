60d6cb175aaaae39b1577c23f561e19e78c09195
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-09 21:52:05 -0300

    wip

---
+++ config/Seeds/UserSeed.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractSeed;
+
+/**
+ * User seed.
+ */
+class UserSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeds is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        $data = [];
+
+        $table = $this->table('users');
+        $table->insert($data)->save();
+    }
+}
