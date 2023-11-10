28fc12cfb67a411afffbd81ac202347b400ceec2
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-09 22:29:20 -0300

    wip

--- config/Seeds/DatabaseSeed.php
+++ config/Seeds/DatabaseSeed.php
@@ -3,26 +3,11 @@ declare(strict_types=1);
 
 use Migrations\AbstractSeed;
 
-/**
- * Database seed.
- */
 class DatabaseSeed extends AbstractSeed
 {
-    /**
-     * Run Method.
-     *
-     * Write your database seeder using this method.
-     *
-     * More information on writing seeds is available here:
-     * https://book.cakephp.org/phinx/0/en/seeding.html
-     *
-     * @return void
-     */
     public function run(): void
     {
-        $data = [];
-
-        $table = $this->table('databases');
-        $table->insert($data)->save();
+       $this->call(UserSeed::class);
+       $this->call(PostSeed::class);
     }
 }
