c80690535c98002c4d6456fc26faed5a98f7f576
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-10 16:31:40 -0300

    wip

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,10 +3,10 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-/**
- * Home Controller
- *
- */
 class HomeController extends AppController
 {
+    public function index() 
+    {
+        dd('top');
+    }
 }
