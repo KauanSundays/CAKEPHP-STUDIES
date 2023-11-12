b4cbe2373039af9700eb9d1f5ba373f31f97029b
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-11 22:13:11 -0300

    wip

--- config/routes.php
+++ config/routes.php
@@ -3,17 +3,10 @@ use Cake\Routing\Route\DashedRoute;
 use Cake\Routing\RouteBuilder;
 
 return function (RouteBuilder $routes): void {
-
-    $routes->setRouteClass(DashedRoute::class);
-
     $routes->scope('/', function (RouteBuilder $builder): void {
-
         $builder->connect('/teste', ['controller' => 'Home', 'action' => 'index']);
         //controller => NOME_DO__CONTROLLER, ACTION => NOME_DO_METODO
-
-        $builder->connect('/pages/*', 'Pages::display');
-
-        $builder->fallbacks();
     });
 
+    $routes->connect('/about', ['controller' => 'About', 'action' => 'index']);
 };
