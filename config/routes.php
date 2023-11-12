234fa5f8ae8368455c5bf13353c191683f9404a1
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-11 22:24:10 -0300

    wip

--- config/routes.php
+++ config/routes.php
@@ -4,7 +4,8 @@ use Cake\Routing\RouteBuilder;
 
 return function (RouteBuilder $routes): void {
     $routes->scope('/', function (RouteBuilder $builder): void {
-        $builder->connect('/teste', ['controller' => 'Home', 'action' => 'index']);
+        $builder->connect('/teste', ['controller' => 'Home', 'action' => 'index'],
+        options: ['_name' => 'home.index']);
         //controller => NOME_DO__CONTROLLER, ACTION => NOME_DO_METODO
     });
 
