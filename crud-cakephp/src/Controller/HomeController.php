623b219012bb281ec603e32aacd6c1a87a8c3fc0
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-14 21:50:35 -0300

    wip

---
+++ crud-cakephp/src/Controller/HomeController.php
@@ -0,0 +1,104 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+/**
+ * Home Controller
+ *
+ * @method \App\Model\Entity\Home[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class HomeController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function index()
+    {
+        $home = $this->paginate($this->Home);
+
+        $this->set(compact('home'));
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Home id.
+     * @return \Cake\Http\Response|null|void Renders view
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $home = $this->Home->get($id, [
+            'contain' => [],
+        ]);
+
+        $this->set(compact('home'));
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $home = $this->Home->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $home = $this->Home->patchEntity($home, $this->request->getData());
+            if ($this->Home->save($home)) {
+                $this->Flash->success(__('The home has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The home could not be saved. Please, try again.'));
+        }
+        $this->set(compact('home'));
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Home id.
+     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $home = $this->Home->get($id, [
+            'contain' => [],
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $home = $this->Home->patchEntity($home, $this->request->getData());
+            if ($this->Home->save($home)) {
+                $this->Flash->success(__('The home has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The home could not be saved. Please, try again.'));
+        }
+        $this->set(compact('home'));
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Home id.
+     * @return \Cake\Http\Response|null|void Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $home = $this->Home->get($id);
+        if ($this->Home->delete($home)) {
+            $this->Flash->success(__('The home has been deleted.'));
+        } else {
+            $this->Flash->error(__('The home could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+}
