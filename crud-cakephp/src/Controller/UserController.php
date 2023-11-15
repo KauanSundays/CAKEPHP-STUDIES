5ab1e900fe61ba1424aff857dfc90226ae18bab5
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-15 09:12:17 -0300

    wip

--- crud-cakephp/src/Controller/UserController.php
+++ crud-cakephp/src/Controller/UserController.php
@@ -3,11 +3,14 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Cake\ORM\TableRegistry;
+
 class UserController extends AppController
 {
     public function index()
     {
-        $users = $this->Users->find()->all();
-        $this->set(['users' => $users]);
+        $usersTable = TableRegistry::getTableLocator()->get('Users');
+        $users = $usersTable->find()->all();
+        $this->set('users', $users);
     }
 }
