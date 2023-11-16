8d567fee910c6fde80e4b8e3dd125810b08bb4dd
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-15 21:27:50 -0300

    wip

--- crud-cakephp/templates/User/index.php
+++ crud-cakephp/templates/User/index.php
@@ -1,3 +1,18 @@
-<?php
 
-var_dump($users);
+<!-- var_dump($users); -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <?php
+        foreach($users as $user)
+        ?>
+    </ul>
+</body>
+</html>
