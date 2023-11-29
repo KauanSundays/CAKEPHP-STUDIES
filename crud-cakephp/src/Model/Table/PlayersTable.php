baf8642f82b561fc54d4b613f74c5f64de01a5be
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-28 22:53:43 -0300

    wip

--- crud-cakephp/src/Model/Table/PlayersTable.php
+++ crud-cakephp/src/Model/Table/PlayersTable.php
@@ -1,80 +1,21 @@
 <?php
 declare(strict_types=1);
 
-namespace App\Model\Table;
+use Migrations\AbstractMigration;
 
-use Cake\ORM\Query;
-use Cake\ORM\RulesChecker;
-use Cake\ORM\Table;
-use Cake\Validation\Validator;
-
-/**
- * Players Model
- *
- * @method \App\Model\Entity\Player newEmptyEntity()
- * @method \App\Model\Entity\Player newEntity(array $data, array $options = [])
- * @method \App\Model\Entity\Player[] newEntities(array $data, array $options = [])
- * @method \App\Model\Entity\Player get($primaryKey, $options = [])
- * @method \App\Model\Entity\Player findOrCreate($search, ?callable $callback = null, $options = [])
- * @method \App\Model\Entity\Player patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
- * @method \App\Model\Entity\Player[] patchEntities(iterable $entities, array $data, array $options = [])
- * @method \App\Model\Entity\Player|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
- * @method \App\Model\Entity\Player saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
- * @method \App\Model\Entity\Player[]|\Cake\Datasource\ResultSetInterface|false saveMany(iterable $entities, $options = [])
- * @method \App\Model\Entity\Player[]|\Cake\Datasource\ResultSetInterface saveManyOrFail(iterable $entities, $options = [])
- * @method \App\Model\Entity\Player[]|\Cake\Datasource\ResultSetInterface|false deleteMany(iterable $entities, $options = [])
- * @method \App\Model\Entity\Player[]|\Cake\Datasource\ResultSetInterface deleteManyOrFail(iterable $entities, $options = [])
- */
-class PlayersTable extends Table
+class PlayersTable extends AbstractMigration
 {
-    /**
-     * Initialize method
-     *
-     * @param array $config The configuration for the Table.
-     * @return void
-     */
-    public function initialize(array $config): void
+    public function change(): void
     {
-        parent::initialize($config);
+        $table = $this->table('players');
 
-        $this->setTable('players');
-        $this->setDisplayField('name');
-        $this->setPrimaryKey('id');
-    }
-
-    /**
-     * Default validation rules.
-     *
-     * @param \Cake\Validation\Validator $validator Validator instance.
-     * @return \Cake\Validation\Validator
-     */
-    public function validationDefault(Validator $validator): Validator
-    {
-        $validator
-            ->scalar('name')
-            ->maxLength('name', 30)
-            ->requirePresence('name', 'create')
-            ->notEmptyString('name');
-
-        $validator
-            ->scalar('position')
-            ->maxLength('position', 15)
-            ->notEmptyString('position');
-
-        return $validator;
-    }
-
-    /**
-     * Returns a rules checker object that will be used for validating
-     * application integrity.
-     *
-     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
-     * @return \Cake\ORM\RulesChecker
-     */
-    public function buildRules(RulesChecker $rules): RulesChecker
-    {
-        $rules->add($rules->isUnique(['id']), ['errorField' => 'id']);
-
-        return $rules;
+        $table
+            ->addColumn('nome', 'string', [
+                'limit' => 30,
+                'default' => null,
+                'null' => false,
+            ])
+            ->addIndex(['id'], ['unique' => true])
+            ->create();
     }
 }
