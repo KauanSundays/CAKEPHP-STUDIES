d6f247314732f7b8d2dc855bbdfd4b32a0b82ff8
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-28 22:54:23 -0300

    correct

--- crud-cakephp/src/Model/Table/PlayersTable.php
+++ crud-cakephp/src/Model/Table/PlayersTable.php
@@ -1,21 +1,28 @@
+// src/Model/Table/PlayersTable.php
 <?php
 declare(strict_types=1);
 
-use Migrations\AbstractMigration;
+namespace App\Model\Table;
 
-class PlayersTable extends AbstractMigration
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+class PlayersTable extends Table
 {
-    public function change(): void
+    public function initialize(array $config): void
     {
-        $table = $this->table('players');
+        parent::initialize($config);
 
-        $table
-            ->addColumn('nome', 'string', [
-                'limit' => 30,
-                'default' => null,
-                'null' => false,
-            ])
-            ->addIndex(['id'], ['unique' => true])
-            ->create();
+        $this->setTable('players');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        // Adicione outras configurações da tabela, se necessário.
+    }
+
+    public function validationDefault(Validator $validator): Validator
+    {
+        // Adicione regras de validação, se necessário.
+        return $validator;
     }
 }
