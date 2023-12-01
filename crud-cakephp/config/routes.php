dddb9e9a8860d6ec67683e0c83e92872c33b1737
kauan <ricardo.kauand@gmail.com>
2023-12-01 16:57:50 -0300

    wip

--- crud-cakephp/config/routes.php
+++ crud-cakephp/config/routes.php
@@ -1,15 +1,10 @@
 <?php
+// config/routes.php
 
 use Cake\Routing\Route\DashedRoute;
 use Cake\Routing\RouteBuilder;
 
 return function (RouteBuilder $routes): void {
-    // $routes->scope('/', function (RouteBuilder $builder): void {
-    //     $builder->connect('/teste', ['controller' => 'Home', 'action' => 'index'],
-    //     options: ['_name' => 'home.index']);
-    //     //controller => NOME_DO__CONTROLLER, ACTION => NOME_DO_METODO
-    // });
-
     $routes->get(
         '/',['controller' => 'Players', 'action' => 'index'], 'Players.index'
     );
@@ -19,9 +14,8 @@ return function (RouteBuilder $routes): void {
     );
     
     $routes->post(
-        '/delete', ['controller' => 'Players', 'action' => 'delete'], 'Players.delete'
+        '/delete/:id', ['controller' => 'Players', 'action' => 'delete'], 'Players.delete'
     );
-    
 
     $routes->get('/edit/:id', ['controller' => 'Players', 'action' => 'edit'], 'Players.edit')
         ->setPatterns(['id' => '\d+'])
