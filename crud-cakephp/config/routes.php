22091d6174a42b9e79a0be64aee32842c924de1a
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-12-03 22:26:55 -0300

    comments

--- crud-cakephp/config/routes.php
+++ crud-cakephp/config/routes.php
@@ -7,14 +7,19 @@ return function (RouteBuilder $routes): void {
 
     $routes->scope('/', function ($routes) {
         $routes->connect('/', ['controller' => 'Players', 'action' => 'index'], ['_name' => 'Players.index']);
+
+        // Pages
         $routes->connect('/add', ['controller' => 'Players', 'action' => 'store'], ['_name' => 'Players.store']);
         $routes->connect('/edit/:id', ['controller' => 'Players', 'action' => 'edit'], ['_name' => 'Players.edit'])
             ->setPatterns(['id' => '\d+'])
             ->setPass(['id']);
+
+        // Delete Action
         $routes->connect('/delete/:id', ['controller' => 'Players', 'action' => 'delete'], ['_name' => 'Players.delete'])
             ->setPatterns(['id' => '\d+'])
             ->setPass(['id']);
 
+        // Register positions for players
         $routes->connect('/register-positions', ['controller' => 'Positions', 'action' => 'index'],
         ['__name' => 'Positions.index']);
     });
