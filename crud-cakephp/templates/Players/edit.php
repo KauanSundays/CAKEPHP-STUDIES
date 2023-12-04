3aad4efd5fe931f712204569f51bb2af637cf026
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-12-03 22:29:22 -0300

    wip

--- crud-cakephp/templates/Players/edit.php
+++ crud-cakephp/templates/Players/edit.php
@@ -1,1 +1,20 @@
-paissanduuu
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Player {{}}</title>
+</head>
+<body>
+    <div>
+        <?= $this->Form->create(null, ['url' => ['controller' => 'Players', 'action' => 'edit']]) ?>
+            <?= $this->Form->hidden('_csrfToken', ['value' => $this->request->getAttribute('csrfToken')]) ?>
+            <label for="name">Name:</label>
+            <input type="text" required id="name" name="name">
+            <label for="position">Position:</label>
+            <input type="text" required id="position" name="position">
+            <input type="submit" value="Edit">
+        <?= $this->Form->end() ?>
+    </div>
+</body>
+</html>
