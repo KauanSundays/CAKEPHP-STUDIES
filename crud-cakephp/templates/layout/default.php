37c459538f17c97a8fc4f31e2737e19bd094ac6a
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-12-03 22:25:40 -0300

    migration create

--- crud-cakephp/templates/layout/default.php
+++ crud-cakephp/templates/layout/default.php
@@ -39,7 +39,7 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
             <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
         </div>
         <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="">Register Positions</a>
+            <a target="_blank" rel="noopener" href="/register-positions">Register Positions</a>
             <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
         </div>
     </nav>
