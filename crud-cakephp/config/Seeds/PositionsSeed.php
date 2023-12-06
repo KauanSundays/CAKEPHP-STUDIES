1ce9cf69a8b391bc4f98da9438189e5430f80d6d
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-12-05 21:57:46 -0300

    wip

--- crud-cakephp/config/Seeds/PositionsSeed.php
+++ crud-cakephp/config/Seeds/PositionsSeed.php
@@ -3,15 +3,33 @@ declare(strict_types=1);
 
 use Migrations\AbstractSeed;
 
-/**
- * Positions seed.
- */
+// Positions seed.
+
 class PositionsSeed extends AbstractSeed
 {
 
     public function run(): void
     {
-        $data = [];
+        $data = [
+            [
+                'position' => 'Goleiro',
+            ],
+            [
+                'position' => 'Zagueiro',
+            ],
+            [
+                'position' => 'Lateral',
+            ],
+            [
+                'position' => 'Volante',
+            ],
+            [
+                'position' => 'Meia',
+            ],
+            [
+                'position' => 'Atacante',
+            ],
+        ];
 
         $table = $this->table('positions');
         $table->insert($data)->save();
