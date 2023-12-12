dc261ed6efca08bbba0246489a3b7f5f7e36c53e
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-12-11 22:56:05 -0300

    wip

--- crud-cakephp/templates/Players/index.php
+++ crud-cakephp/templates/Players/index.php
@@ -1,9 +1,9 @@
 <h1>Players List</h1>
 
-<?= $this->Form->create(null, ['url' => ['controller' => 'Players', 'action' => 'store']]) ?>
+<?= $this->Form->create(null, ['url' => ['controller' => 'Players', 'action' => 'store'], 'id' => 'playerForm']) ?>
     <?= $this->Form->hidden('_csrfToken', ['value' => $this->request->getAttribute('csrfToken')]) ?>
     <label for="name">Name:</label>
-    <input type="text" required id="name" name="name">
+    <input type="text" id="name" name="name">
     <label for="position">Position:</label>
     <?= $this->Form->select('position', $positions, ['empty' => true, 'id' => 'position']) ?>
 
@@ -22,9 +22,9 @@
             <td><?= $player->name ?></td>
             <td><?= $player->position->position_name ?></td>
             <td>
-            <button type="button" onclick="editAction(<?= $player->id ?>, '<?= $player->name ?>', <?= $player->position->id ?>)">
-                Edit
-            </button>
+                <button type="button" onclick="editAction(<?= $player->id ?>, '<?= $player->name ?>', <?= $player->position->id ?>)">
+                    Edit
+                </button>
             </td>
             <td>
                 <?= $this->Form->postLink(
@@ -39,9 +39,13 @@
 
 <script>
     function editAction(id, player_name, player_position_id) {
-        document.getElementById('id').value = id;
+        console.log(id, player_name, player_position_id);
+
+        // Ajuste a URL de ação do formulário para a URL de edição do jogador específico
+        document.getElementById('playerForm').action = '/players/edit/' + id;
+
+        // Preencha os campos
         document.getElementById('name').value = player_name;
         document.getElementById('position').value = player_position_id;
-        console.log(id, player_name, player_position_id)
     }
 </script>
