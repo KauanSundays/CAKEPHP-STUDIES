74ef5ab7acb7336256fc62622a6a695c255cf8c6
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-01-26 10:07:17 -0300

    wip

--- cake-studies/BancoDeDados-e-Modelos/config/Migrations/20240126125713_CreateProductsTable.php
+++ cake-studies/BancoDeDados-e-Modelos/config/Migrations/20240126125713_CreateProductsTable.php
@@ -5,16 +5,19 @@ use Migrations\AbstractMigration;
 
 class CreateProductsTable extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * More information on this method is available here:
-     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
-     * @return void
-     */
     public function change(): void
     {
         $table = $this->table('products_table');
+        $table->addColumn('name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('price', 'decimal', [
+            'default' => null,
+            'null' => false,
+            'scale' => 2,
+        ]);
         $table->create();
     }
 }
