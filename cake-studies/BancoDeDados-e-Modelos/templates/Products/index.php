8da5e299b991df6f71fd10fa0fddf5beb7e277c2
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-01-30 22:20:41 -0300

    wip

---
+++ cake-studies/BancoDeDados-e-Modelos/templates/Products/index.php
@@ -0,0 +1,15 @@
+<div>
+    <form action="post">
+        <div>
+            <input type="text" name="name" placeholder="Type the name of product">
+        </div>
+        <div>
+            <input type="numeric" name="price" placeholder="Select de price of product">
+        </div>
+        <div>
+            <button>
+                Submit
+            </button>
+        </div>
+    </form>
+</div>
